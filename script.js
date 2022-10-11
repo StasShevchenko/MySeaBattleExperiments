7fe887da8985ffc8ff73537c65cc567b553ad28f
StasShevchenko <dark.knight22832@gmail.com>
2022-10-11 22:48:15 +0300

    Popups functionality was added.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-import * as prepareGameFunctions from './prepare_game_functions.js';
+import * as prepareGameFunctions from "./prepare_game_functions.js";
 let stompClient = null;
 let currentPlayerName;
 let currentPlayerId;
+let isIdReceived = false;
 const url = "http://localhost:8080";
 
 const startForm = document.getElementById("startForm");
 const playersListForm = document.getElementById("playersListForm");
 const mainGameForm = document.getElementById("mainGameForm");
 const playersList = document.getElementById("playersList");
-
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -25,15 +25,35 @@ enterNameButton.addEventListener("click", function () {
     welcomeHeader.innerHTML = "Добро пожаловать " + currentPlayerName + " !";
     const socket = new SockJS(url + "/seabattle");
     stompClient = Stomp.over(socket);
-    stompClient.connect(
-      {},
-      onConnectSuccess
-    );
+    stompClient.connect({}, onConnectSuccess);
   }
 });
 
 //
 //Конец блока входа в игру
+//
+
+//////////////////////////////////////////////////////////////////////////////////
+
+//
+//Блок работы со списком игроков
+//
+
+const invitePopUp = document.getElementById("invitePopUpContainer");
+const waitPopUp = document.getElementById("waitPopUpContainer");
+const acceptInvitationButton = document.getElementById(
+  "acceptInvitationButton"
+);
+const rejectInvitationButton = document.getElementById(
+  "rejectInvitationButton"
+);
+const invitationHeader = document.getElementById("invitationHeader");
+const rejectSendedInvitationButton = document.getElementById(
+  "rejectSendedInvitationButton"
+);
+
+//
+//Конец блока работы со списком игроков
 //
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -87,53 +107,115 @@ for (let radio of orientationRadios) {
 
 function onPlayersReceive(payload) {
   const players = JSON.parse(payload.body);
-  while(playersList.firstChild){
+  while (playersList.firstChild) {
     playersList.removeChild(playersList.lastChild);
   }
-  for(let player of players){
-    if(player.login.trim() != currentPlayerName){
+  for (let player of players) {
+    if (player.login.trim() != currentPlayerName) {
       const playerItem = document.createElement("li");
       playerItem.appendChild(document.createTextNode(player.login));
       const playerInviteButton = document.createElement("button");
       playerInviteButton.textContent = "Бросить вызов!";
       playerInviteButton.style.marginLeft = "10px";
-      playerInviteButton.addEventListener("click", function(){
+      playerInviteButton.addEventListener("click", function () {
         sendInviteToPlayer(player);
-      })
+      });
       playerItem.appendChild(playerInviteButton);
-      playersList.appendChild(playerItem); 
-    } else {
-      currentPlayerId = player.id;
-      subscribeToPrivateMessages();
+      playersList.appendChild(playerItem);
+    }
+  }
+  if(!isIdReceived){
+    for(let i = players.length - 1; i >= 0 && !isIdReceived; i --){
+
+      if(players[i].login.trim() == currentPlayerName){
+        console.log("I've get id: " + players[i].id.trim())
+        isIdReceived = true;
+        currentPlayerId = players[i].id.trim();
+        subscribeToPrivateMessages();
+      }
     }
   }
 }
 
-function subscribeToPrivateMessages(){
-  stompClient.subscribe("/private/messages" + currentPlayerId.trim(), function(payload){
-    console.log("receved value!!!");
-    console.log("I've received invite: " + payload);
-  })
+
+//Подписываемся на приватные сообщения. Обработка всех приходящих сообщения
+function subscribeToPrivateMessages() {
+  stompClient.subscribe(
+    //Все сообщения отправленные на /private-message переправляются сервером в пути с конкретным id
+    "/private/messages" + currentPlayerId.trim(),
+    function (payload) {
+      let message = JSON.parse(payload.body);
+      //Если пришло приглашение
+      if (message.message == "INVITE") {
+        console.log(message.senderId);
+        invitePopUp.style.display = "flex";
+        invitationHeader.innerHTML =
+          "Игрок " + message.senderName + " бросил вам вызов!";
+          //Отказываемся от приглашения
+        rejectInvitationButton.addEventListener("click", function () {
+          invitePopUp.style.display = "none";
+          console.log(message.senderId)
+          stompClient.send("/app/private-message",
+          {},
+          JSON.stringify({
+            message: "REJECT",
+            receiverId: message.senderId,
+            senderId: currentPlayerId,
+            senderName: currentPlayerName,
+          }))
+        });
+      }
+      //Если отправитель отменил свое приглашение
+      if (message.message == "CANCEL_INVITATION") {
+        invitePopUp.style.display = "none";
+      }
+      //Если получатель не принял приглашение
+      if(message.message == "REJECT"){
+        waitPopUp.style.display = "none";
+      }
+    }
+  );
 }
 
-function sendInviteToPlayer(player){
-  let message = "Hello man";
+
+//Функция для отправки сообщения игроку
+// и назначение кнопки отмены
+function sendInviteToPlayer(player) {
+  let message = "INVITE";
+  let cancelMessage = "CANCEL_INVITATION";
+  waitPopUp.style.display = "flex";
+  //Отправка отмены приглашения
+  rejectSendedInvitationButton.addEventListener("click", function (player) {
+    waitPopUp.style.display = "none"
+    stompClient.send(
+      "/app/private-message",
+      {},
+      JSON.stringify({
+        message: cancelMessage,
+        receiverId: player.id,
+        senderId: currentPlayerId,
+        senderName: currentPlayerName,
+      })
+    );
+  });
+  //Отправка приглашения
   stompClient.send(
     "/app/private-message",
     {},
-    JSON.stringify({message:message, receiverId: player.id})
-  )
+    JSON.stringify({
+      message: message,
+      receiverId: player.id,
+      senderId: currentPlayerId,
+      senderName: currentPlayerName,
+    })
+  );
 }
 
-function onConnectSuccess(){
+function onConnectSuccess() {
   stompClient.subscribe("/topic/players", function (payload) {
     onPlayersReceive(payload);
   });
-  stompClient.send(
-    "/app/addplayer",
-    {},
-    enterNameTextField.value.trim()
-  );
+  stompClient.send("/app/addplayer", {}, enterNameTextField.value.trim());
   startForm.style.display = "none";
   playersListForm.style.display = "block";
 }
